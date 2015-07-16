bafe14043ee1bba633bb6f2ee15d2a4177daea34
foclgithub <zhatao_fljd@163.com>
2015-07-16 21:31:41 +0800

    忘了上传文件DrawROI
    Signed-off-by: foclgithub <zhatao_fljd@163.com>

---
+++ BLTransfusion/BLTransfusion/DrawROI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HalconDotNet;
+
+namespace BLTransfusion
+{
+    public class DrawROI
+    {
+        public static RectangleInfo DrawRectangle(HWindow windowHandle)
+        {
+            double hv_Row1, hv_Column1, hv_Row2, hv_Column2;
+            windowHandle.SetLineWidth(2);
+            windowHandle.SetDraw("margin");
+            windowHandle.SetColor("red");
+
+            windowHandle.DrawRectangle1(out hv_Row1, out hv_Column1, out hv_Row2, out hv_Column2);
+
+            HObject ho_Rectangle;
+            HOperatorSet.GenRectangle1(out ho_Rectangle, hv_Row1, hv_Column1, hv_Row2, hv_Column2);
+
+            windowHandle.SetLineWidth(3);
+            windowHandle.SetDraw("margin");
+            windowHandle.SetColor("blue");
+            windowHandle.DispObj(ho_Rectangle);
+
+            var rectInfo = new RectangleInfo()
+            {
+                RowTop = hv_Row1,
+                ColumnLeft = hv_Column1,
+                RowBottom = hv_Row2,
+                ColumnRight = hv_Column2,
+            };
+            return rectInfo;
+        }
+    }
+}
