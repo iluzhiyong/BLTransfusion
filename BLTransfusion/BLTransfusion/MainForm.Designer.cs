715b3b5a84e635aa0f1f81064ae9af50fd15ea74
iluzhiyong <iluzhiyong@163.com>
2015-10-14 21:16:54 +0800

    图标修改为32*32大小

--- BLTransfusion/BLTransfusion/MainForm.Designer.cs
+++ BLTransfusion/BLTransfusion/MainForm.Designer.cs
@@ -78,22 +78,25 @@
             // TsCameraStatus
             // 
             this.TsCameraStatus.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.TsCameraStatus.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TsCameraStatus.Name = "TsCameraStatus";
-            this.TsCameraStatus.Size = new System.Drawing.Size(69, 17);
+            this.TsCameraStatus.Size = new System.Drawing.Size(74, 17);
             this.TsCameraStatus.Text = "相机：关闭";
             // 
             // TsQualifiedCntTitle
             // 
             this.TsQualifiedCntTitle.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.TsQualifiedCntTitle.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TsQualifiedCntTitle.Name = "TsQualifiedCntTitle";
-            this.TsQualifiedCntTitle.Size = new System.Drawing.Size(63, 17);
+            this.TsQualifiedCntTitle.Size = new System.Drawing.Size(68, 17);
             this.TsQualifiedCntTitle.Text = "合格数： ";
             // 
             // TsUnqualifiedCntTitle
             // 
             this.TsUnqualifiedCntTitle.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.TsUnqualifiedCntTitle.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TsUnqualifiedCntTitle.Name = "TsUnqualifiedCntTitle";
-            this.TsUnqualifiedCntTitle.Size = new System.Drawing.Size(75, 17);
+            this.TsUnqualifiedCntTitle.Size = new System.Drawing.Size(81, 17);
             this.TsUnqualifiedCntTitle.Text = "不合格数： ";
             // 
             // statusStrip1
@@ -119,32 +122,36 @@
             // TsUartStatus
             // 
             this.TsUartStatus.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.TsUartStatus.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TsUartStatus.Name = "TsUartStatus";
-            this.TsUartStatus.Size = new System.Drawing.Size(69, 17);
+            this.TsUartStatus.Size = new System.Drawing.Size(74, 17);
             this.TsUartStatus.Text = "串口：关闭";
             // 
             // toolStripStatusLabel1
             // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(588, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(608, 17);
             this.toolStripStatusLabel1.Spring = true;
             // 
             // TsQualifiedCnt
             // 
+            this.TsQualifiedCnt.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TsQualifiedCnt.Name = "TsQualifiedCnt";
-            this.TsQualifiedCnt.Size = new System.Drawing.Size(11, 17);
+            this.TsQualifiedCnt.Size = new System.Drawing.Size(12, 17);
             this.TsQualifiedCnt.Text = "0";
             // 
             // TsUnqualifiedCnt
             // 
+            this.TsUnqualifiedCnt.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TsUnqualifiedCnt.Name = "TsUnqualifiedCnt";
-            this.TsUnqualifiedCnt.Size = new System.Drawing.Size(11, 17);
+            this.TsUnqualifiedCnt.Size = new System.Drawing.Size(12, 17);
             this.TsUnqualifiedCnt.Text = "0";
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.SystemColors.Menu;
-            this.menuStrip1.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.menuStrip1.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.相机ToolStripMenuItem,
@@ -168,39 +175,39 @@
             this.toolStripMenuItem4,
             this.cameraSettingMenuItem});
             this.相机ToolStripMenuItem.Name = "相机ToolStripMenuItem";
-            this.相机ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.相机ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
             this.相机ToolStripMenuItem.Text = "相机";
             // 
             // openCameraMenuItem
             // 
             this.openCameraMenuItem.Name = "openCameraMenuItem";
-            this.openCameraMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.openCameraMenuItem.Size = new System.Drawing.Size(122, 22);
             this.openCameraMenuItem.Text = "启动相机";
             this.openCameraMenuItem.Click += new System.EventHandler(this.openCameraMenuItem_Click);
             // 
             // snapshotMenuItem
             // 
             this.snapshotMenuItem.Name = "snapshotMenuItem";
-            this.snapshotMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.snapshotMenuItem.Size = new System.Drawing.Size(122, 22);
             this.snapshotMenuItem.Text = "拍摄照片";
             this.snapshotMenuItem.Click += new System.EventHandler(this.snapshotMenuItem_Click);
             // 
             // closeCameraMenuItem
             // 
             this.closeCameraMenuItem.Name = "closeCameraMenuItem";
-            this.closeCameraMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.closeCameraMenuItem.Size = new System.Drawing.Size(122, 22);
             this.closeCameraMenuItem.Text = "关闭相机";
             this.closeCameraMenuItem.Click += new System.EventHandler(this.closeCameraMenuItem_Click);
             // 
             // toolStripMenuItem4
             // 
             this.toolStripMenuItem4.Name = "toolStripMenuItem4";
-            this.toolStripMenuItem4.Size = new System.Drawing.Size(115, 6);
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(119, 6);
             // 
             // cameraSettingMenuItem
             // 
             this.cameraSettingMenuItem.Name = "cameraSettingMenuItem";
-            this.cameraSettingMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.cameraSettingMenuItem.Size = new System.Drawing.Size(122, 22);
             this.cameraSettingMenuItem.Text = "相机设定";
             this.cameraSettingMenuItem.Click += new System.EventHandler(this.cameraSettingMenuItem_Click);
             // 
@@ -214,46 +221,46 @@
             this.clearRecordMenuItem,
             this.MenuImgProcAlgorithmSet});
             this.MenuItemImageProcOpen.Name = "MenuItemImageProcOpen";
-            this.MenuItemImageProcOpen.Size = new System.Drawing.Size(65, 20);
+            this.MenuItemImageProcOpen.Size = new System.Drawing.Size(69, 20);
             this.MenuItemImageProcOpen.Text = "图像处理";
             // 
             // manualDetect
             // 
             this.manualDetect.Name = "manualDetect";
-            this.manualDetect.Size = new System.Drawing.Size(118, 22);
+            this.manualDetect.Size = new System.Drawing.Size(122, 22);
             this.manualDetect.Text = "手动检测";
             this.manualDetect.Click += new System.EventHandler(this.manualDetect_Click);
             // 
             // autoDetect
             // 
             this.autoDetect.Name = "autoDetect";
-            this.autoDetect.Size = new System.Drawing.Size(118, 22);
+            this.autoDetect.Size = new System.Drawing.Size(122, 22);
             this.autoDetect.Text = "自动检测";
             this.autoDetect.Click += new System.EventHandler(this.autoDetect_Click);
             // 
             // stopDetect
             // 
             this.stopDetect.Name = "stopDetect";
-            this.stopDetect.Size = new System.Drawing.Size(118, 22);
+            this.stopDetect.Size = new System.Drawing.Size(122, 22);
             this.stopDetect.Text = "停止检测";
             this.stopDetect.Click += new System.EventHandler(this.stopDetect_Click);
             // 
             // toolStripMenuItem3
             // 
             this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(115, 6);
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(119, 6);
             // 
             // clearRecordMenuItem
             // 
             this.clearRecordMenuItem.Name = "clearRecordMenuItem";
-            this.clearRecordMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.clearRecordMenuItem.Size = new System.Drawing.Size(122, 22);
             this.clearRecordMenuItem.Text = "清空记录";
             this.clearRecordMenuItem.Click += new System.EventHandler(this.clearRecordMenuItem_Click);
             // 
             // MenuImgProcAlgorithmSet
             // 
             this.MenuImgProcAlgorithmSet.Name = "MenuImgProcAlgorithmSet";
-            this.MenuImgProcAlgorithmSet.Size = new System.Drawing.Size(118, 22);
+            this.MenuImgProcAlgorithmSet.Size = new System.Drawing.Size(122, 22);
             this.MenuImgProcAlgorithmSet.Text = "算法设置";
             this.MenuImgProcAlgorithmSet.Click += new System.EventHandler(this.MenuImgProcAlgorithmSet_Click);
             // 
@@ -265,34 +272,34 @@
             this.MenuOpenRelay,
             this.MenuCloseRelay});
             this.iOToolStripMenuItem.Name = "iOToolStripMenuItem";
-            this.iOToolStripMenuItem.Size = new System.Drawing.Size(29, 20);
+            this.iOToolStripMenuItem.Size = new System.Drawing.Size(31, 20);
             this.iOToolStripMenuItem.Text = "IO";
             // 
             // MenuOpenUART
             // 
             this.MenuOpenUART.Name = "MenuOpenUART";
-            this.MenuOpenUART.Size = new System.Drawing.Size(130, 22);
+            this.MenuOpenUART.Size = new System.Drawing.Size(135, 22);
             this.MenuOpenUART.Text = "打开串口";
             this.MenuOpenUART.Click += new System.EventHandler(this.MenuOpenUART_Click);
             // 
             // MenuCloseUART
             // 
             this.MenuCloseUART.Name = "MenuCloseUART";
-            this.MenuCloseUART.Size = new System.Drawing.Size(130, 22);
+            this.MenuCloseUART.Size = new System.Drawing.Size(135, 22);
             this.MenuCloseUART.Text = "关闭串口";
             this.MenuCloseUART.Click += new System.EventHandler(this.MenuCloseUART_Click);
             // 
             // MenuOpenRelay
             // 
             this.MenuOpenRelay.Name = "MenuOpenRelay";
-            this.MenuOpenRelay.Size = new System.Drawing.Size(130, 22);
+            this.MenuOpenRelay.Size = new System.Drawing.Size(135, 22);
             this.MenuOpenRelay.Text = "打开继电器";
             this.MenuOpenRelay.Click += new System.EventHandler(this.MenuOpenRelay_Click);
             // 
             // MenuCloseRelay
             // 
             this.MenuCloseRelay.Name = "MenuCloseRelay";
-            this.MenuCloseRelay.Size = new System.Drawing.Size(130, 22);
+            this.MenuCloseRelay.Size = new System.Drawing.Size(135, 22);
             this.MenuCloseRelay.Text = "关闭继电器";
             this.MenuCloseRelay.Click += new System.EventHandler(this.MenuCloseRelay_Click);
             // 
@@ -303,33 +310,34 @@
             this.WndLayoutVertical,
             this.WndLayoutCascade});
             this.WndLayout.Name = "WndLayout";
-            this.WndLayout.Size = new System.Drawing.Size(41, 20);
+            this.WndLayout.Size = new System.Drawing.Size(43, 20);
             this.WndLayout.Text = "窗口";
             // 
             // WndLayoutHorizontal
             // 
             this.WndLayoutHorizontal.Name = "WndLayoutHorizontal";
-            this.WndLayoutHorizontal.Size = new System.Drawing.Size(118, 22);
+            this.WndLayoutHorizontal.Size = new System.Drawing.Size(122, 22);
             this.WndLayoutHorizontal.Text = "水平平铺";
             this.WndLayoutHorizontal.Click += new System.EventHandler(this.WndLayoutHorizontal_Click);
             // 
             // WndLayoutVertical
             // 
             this.WndLayoutVertical.Name = "WndLayoutVertical";
-            this.WndLayoutVertical.Size = new System.Drawing.Size(118, 22);
+            this.WndLayoutVertical.Size = new System.Drawing.Size(122, 22);
             this.WndLayoutVertical.Text = "垂直平铺";
             this.WndLayoutVertical.Click += new System.EventHandler(this.WndLayoutVertical_Click);
             // 
             // WndLayoutCascade
             // 
             this.WndLayoutCascade.Name = "WndLayoutCascade";
-            this.WndLayoutCascade.Size = new System.Drawing.Size(118, 22);
+            this.WndLayoutCascade.Size = new System.Drawing.Size(122, 22);
             this.WndLayoutCascade.Text = "层叠排列";
             this.WndLayoutCascade.Click += new System.EventHandler(this.WndLayoutCascade_Click);
             // 
             // toolStrip1
             // 
             this.toolStrip1.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton_ConnectCamera,
             this.toolStripButton_Snap,
@@ -340,7 +348,7 @@
             this.toolStripButton_ClearResult});
             this.toolStrip1.Location = new System.Drawing.Point(0, 24);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(944, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(944, 39);
             this.toolStrip1.TabIndex = 13;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -350,7 +358,7 @@
             this.toolStripButton_ConnectCamera.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_ConnectCamera.Image")));
             this.toolStripButton_ConnectCamera.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_ConnectCamera.Name = "toolStripButton_ConnectCamera";
-            this.toolStripButton_ConnectCamera.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_ConnectCamera.Size = new System.Drawing.Size(36, 36);
             this.toolStripButton_ConnectCamera.Text = "toolStripButton_ConnectCame";
             this.toolStripButton_ConnectCamera.ToolTipText = "启动相机";
             this.toolStripButton_ConnectCamera.Click += new System.EventHandler(this.toolStripButton_ConnectCamera_Click);
@@ -361,7 +369,7 @@
             this.toolStripButton_Snap.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_Snap.Image")));
             this.toolStripButton_Snap.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_Snap.Name = "toolStripButton_Snap";
-            this.toolStripButton_Snap.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_Snap.Size = new System.Drawing.Size(36, 36);
             this.toolStripButton_Snap.Text = "toolStripButton_Snap";
             this.toolStripButton_Snap.ToolTipText = "拍摄照片";
             this.toolStripButton_Snap.Click += new System.EventHandler(this.toolStripButton_Snap_Click);
@@ -372,7 +380,7 @@
             this.toolStripButton_CameraSet.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_CameraSet.Image")));
             this.toolStripButton_CameraSet.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_CameraSet.Name = "toolStripButton_CameraSet";
-            this.toolStripButton_CameraSet.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_CameraSet.Size = new System.Drawing.Size(36, 36);
             this.toolStripButton_CameraSet.Text = "toolStripButton_CameraSet";
             this.toolStripButton_CameraSet.ToolTipText = "相机设定";
             this.toolStripButton_CameraSet.Click += new System.EventHandler(this.toolStripButton_CameraSet_Click);
@@ -380,7 +388,7 @@
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
             // 
             // toolStripButton_AutoStart
             // 
@@ -388,7 +396,7 @@
             this.toolStripButton_AutoStart.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_AutoStart.Image")));
             this.toolStripButton_AutoStart.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_AutoStart.Name = "toolStripButton_AutoStart";
-            this.toolStripButton_AutoStart.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_AutoStart.Size = new System.Drawing.Size(36, 36);
             this.toolStripButton_AutoStart.Text = "toolStripButton_AutoStart";
             this.toolStripButton_AutoStart.ToolTipText = "自动检测";
             this.toolStripButton_AutoStart.Click += new System.EventHandler(this.toolStripButton_AutoStart_Click);
@@ -399,7 +407,7 @@
             this.toolStripButton_AutoStop.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_AutoStop.Image")));
             this.toolStripButton_AutoStop.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_AutoStop.Name = "toolStripButton_AutoStop";
-            this.toolStripButton_AutoStop.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_AutoStop.Size = new System.Drawing.Size(36, 36);
             this.toolStripButton_AutoStop.Text = "toolStripButton_AutoStop";
             this.toolStripButton_AutoStop.ToolTipText = "停止检测";
             this.toolStripButton_AutoStop.Click += new System.EventHandler(this.toolStripButton_AutoStop_Click);
@@ -410,7 +418,7 @@
             this.toolStripButton_ClearResult.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_ClearResult.Image")));
             this.toolStripButton_ClearResult.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_ClearResult.Name = "toolStripButton_ClearResult";
-            this.toolStripButton_ClearResult.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_ClearResult.Size = new System.Drawing.Size(36, 36);
             this.toolStripButton_ClearResult.Text = "toolStripButton_ClearResult";
             this.toolStripButton_ClearResult.ToolTipText = "清空记录";
             this.toolStripButton_ClearResult.Click += new System.EventHandler(this.toolStripButton_ClearResult_Click);
